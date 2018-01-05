8247e367bd9d8b7ac638c82a448c22fd0cbbb65d
thinkMMP <2271404280@qq.com>
2018-01-05 13:40:08 +0800

    start

--- git_hub.sh
+++ git_hub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # @Date:   2018-01-05 13:33:50
-# @Last Modified time: 2018-01-05 13:39:28
-git status | grep "nothing to commit \| 无文件要提交"
+# @Last Modified time: 2018-01-05 13:40:00
+git status | grep 'nothing to commit \| 无文件要提交'
 echo $?
 # git add --all
 # git commit -m $now
