30852ede65408d84d157a151dc9c4b04ce717506
thinkMMP <2271404280@qq.com>
2018-01-05 13:49:28 +0800

    2018-01-05~13:49:28

--- git_hub.sh
+++ git_hub.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # @Date:   2018-01-05 13:33:50
-# @Last Modified time: 2018-01-05 13:40:32
+# @Last Modified time: 2018-01-05 13:49:22
 git status | grep 'nothing to commit\|无文件要提交'
-echo $?
-# git add --all
-# git commit -m $now
-# git pull origin master
-# git push origin master
+if [ $? != 0 ]; then
+    now=$(date +%Y-%m-%d~%H:%M:%S)
+    git add .
+    git commit -m $now
+    git pull origin master
+    git push origin master
+else
+    git pull origin master
+fi
+echo "update successful"
